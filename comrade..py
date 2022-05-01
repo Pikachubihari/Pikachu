7b6d5836d38da3833dbabbbef6eab2f7dbd2828d
BIHRI BABU <79754454+mafiapikachu@users.noreply.github.com>
2022-05-02 00:39:35 +0530

    Add files via upload

---
+++ comrade..py
@@ -0,0 +1,45 @@
+import logging
+from db import *
+from telethon.sessions import StringSession
+from telethon import TelegramClient, events
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from telethon.tl.functions.channels import GetFullChannelRequest, JoinChannelRequest, InviteToChannelRequest
+import time
+
+logging.basicConfig(level=logging.INFO)
+OWNER_IDS = [2140114063, 2067682959] # edit your id
+client = TelegramClient(
+    StringSession("Your string session here"),
+    api_id=Your api id,
+    api_hash="Your api hash"
+).start()
+# import the time module
+
+
+
+
+
+#-------------Control Panel----------------#
+photo = 'Your Telegraph Link'
+TIME_INTERVAL = 1 #An Integer Always add time in minutes
+OPTION = 2 #1 for send media + Caption and 2 for only message
+
+#-------------------------------------------#
+
+def countdown(t):
+	
+	while t:
+		mins, secs = divmod(t, 60)
+		timer = '{:02d}:{:02d}'.format(mins, secs)
+		print(timer, end="\r")
+		time.sleep(1)
+		t -= 1
+	
+	print('Comrade always OP!!')
+
+
+t = TIME_INTERVAL*1
+
+
+
+  
